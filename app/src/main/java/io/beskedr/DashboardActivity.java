83e9e93ed4ec85bcf6f69e628c75e5637ef170de
Niels Heltner <niels.heltner@hotmail.com>
2018-02-22 23:23:04 +0100

    Tilføjet til + knappen på dashboard

--- app/src/main/java/io/beskedr/DashboardActivity.java
+++ app/src/main/java/io/beskedr/DashboardActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +21,10 @@ public class DashboardActivity extends AppCompatActivity {
         if(messages > 0) {
             ((TextView) findViewById(R.id.noMessages)).setText("");
         }
+    }
+
+    public void newConversation(View view) {
+        Toast.makeText(getApplicationContext(), "New", Toast.LENGTH_SHORT).show();
     }
 
     @Override
