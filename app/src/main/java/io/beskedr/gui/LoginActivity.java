5b4de69497b31e76378e1c15ee4e2cb8fc475b41
Niels Heltner <niels.heltner@hotmail.com>
2018-03-07 17:40:50 +0100

    refactored LoginActivity to ButterKnife annotations

--- app/src/main/java/io/beskedr/gui/LoginActivity.java
+++ app/src/main/java/io/beskedr/gui/LoginActivity.java
@@ -6,27 +6,42 @@ import android.animation.TimeInterpolator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.beskedr.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     public static boolean SHOULD_SHOW_WELCOME_ANIM = true;
+
+    @BindView(R.id.loginUsername) EditText usernameField;
+    @BindView(R.id.loginPassword) EditText passwordField;
+    @BindView(R.id.textInputLayoutLoginUsername) TextInputLayout textInputLayoutUsername;
+    @BindView(R.id.textInputLayoutLoginPassword) TextInputLayout textInputLayoutPassword;
+    @BindView(R.id.loginHeader) EditText loginHeader;
+    @BindView(R.id.loginBtn) Button loginBtn;
+    @BindView(R.id.noAccount) TextView noAccount;
+
     private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
 
         initFieldsFromIntent();
 
@@ -35,10 +50,7 @@ public class LoginActivity extends AppCompatActivity {
             handler.post(new Runnable() {
                 @Override
                 public void run() {
-                    ObjectAnimator headerFadeInAnim = ObjectAnimator.ofFloat(findViewById(R.id.loginHeader), "alpha", 1f);
-                    headerFadeInAnim.setDuration(1000);
-                    headerFadeInAnim.setStartDelay(400);
-                    headerFadeInAnim.start();
+                    animateHeader();
                 }
             });
             handler.postDelayed(new Runnable() {
@@ -59,8 +71,8 @@ public class LoginActivity extends AppCompatActivity {
 
     public void register(View view) {
         Intent registerIntent = new Intent(this, RegisterActivity.class);
-        String username = ((EditText) findViewById(R.id.loginUsername)).getText().toString();
-        String password = ((EditText) findViewById(R.id.loginPassword)).getText().toString();
+        String username = usernameField.getText().toString();
+        String password = passwordField.getText().toString();
 
         registerIntent.putExtra(getString(R.string.EXTRA_USERNAME), username);
         registerIntent.putExtra(getString(R.string.EXTRA_PASSWORD), password);
@@ -73,25 +85,32 @@ public class LoginActivity extends AppCompatActivity {
         String enteredUsername = intent.getStringExtra(getString(R.string.EXTRA_USERNAME));
         String enteredPassword = intent.getStringExtra(getString(R.string.EXTRA_PASSWORD));
 
-        ((EditText) findViewById(R.id.loginUsername)).setText(enteredUsername);
-        ((EditText) findViewById(R.id.loginPassword)).setText(enteredPassword);
+        usernameField.setText(enteredUsername);
+        passwordField.setText(enteredPassword);
     }
 
     private void prepareSceneForWelcomeAnim() {
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
 
-        findViewById(R.id.loginHeader).setY(displayMetrics.heightPixels / 4);
-        ((TextView) findViewById(R.id.loginHeader)).setText(R.string.header_welcome);
-        findViewById(R.id.loginHeader).setAlpha(0f);
-        findViewById(R.id.textInputLayoutLoginUsername).setAlpha(0f);
-        findViewById(R.id.textInputLayoutLoginPassword).setAlpha(0f);
-        findViewById(R.id.loginBtn).setAlpha(0f);
-        findViewById(R.id.noAccount).setAlpha(0f);
+        loginHeader.setY(displayMetrics.heightPixels / 4);
+        loginHeader.setText(R.string.header_welcome);
+        loginHeader.setAlpha(0f);
+        textInputLayoutUsername.setAlpha(0f);
+        textInputLayoutPassword.setAlpha(0f);
+        loginBtn.setAlpha(0f);
+        noAccount.setAlpha(0f);
+    }
+
+    private void animateHeader() {
+        ObjectAnimator headerFadeInAnim = ObjectAnimator.ofFloat(loginHeader, "alpha", 1f);
+        headerFadeInAnim.setDuration(1000);
+        headerFadeInAnim.setStartDelay(400);
+        headerFadeInAnim.start();
     }
 
     private void animate() {
-        ObjectAnimator headerMoveAnim = ObjectAnimator.ofFloat(findViewById(R.id.loginHeader), "translationY", 0f);
+        ObjectAnimator headerMoveAnim = ObjectAnimator.ofFloat(loginHeader, "translationY", 0f);
         headerMoveAnim.setDuration(1500);
         headerMoveAnim.setInterpolator(new TimeInterpolator() {
             FastOutSlowInInterpolator interpolator = new FastOutSlowInInterpolator();
@@ -101,19 +120,19 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        ObjectAnimator usernameFadeAnim = ObjectAnimator.ofFloat(findViewById(R.id.textInputLayoutLoginUsername), "alpha", 1f);
+        ObjectAnimator usernameFadeAnim = ObjectAnimator.ofFloat(textInputLayoutUsername, "alpha", 1f);
         usernameFadeAnim.setDuration(1000);
         usernameFadeAnim.setStartDelay(600);
 
-        ObjectAnimator passwordFadeAnim = ObjectAnimator.ofFloat(findViewById(R.id.textInputLayoutLoginPassword), "alpha", 1f);
+        ObjectAnimator passwordFadeAnim = ObjectAnimator.ofFloat(textInputLayoutPassword, "alpha", 1f);
         passwordFadeAnim.setDuration(1000);
         passwordFadeAnim.setStartDelay(900);
 
-        ObjectAnimator btnFadeAnim = ObjectAnimator.ofFloat(findViewById(R.id.loginBtn), "alpha", 1f);
+        ObjectAnimator btnFadeAnim = ObjectAnimator.ofFloat(loginBtn, "alpha", 1f);
         btnFadeAnim.setDuration(1000);
         btnFadeAnim.setStartDelay(1800);
 
-        ObjectAnimator noAccountFadeAnim = ObjectAnimator.ofFloat(findViewById(R.id.noAccount), "alpha", 1f);
+        ObjectAnimator noAccountFadeAnim = ObjectAnimator.ofFloat(noAccount, "alpha", 1f);
         noAccountFadeAnim.setDuration(1000);
         noAccountFadeAnim.setStartDelay(1800);
 
@@ -126,7 +145,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override public void onAnimationStart(Animation animation) {}
             @Override public void onAnimationEnd(Animation animation) {}
             @Override public void onAnimationRepeat(Animation animation) {
-                ((TextView) findViewById(R.id.loginHeader)).setText(R.string.header_login);
+                loginHeader.setText(R.string.header_login);
             }
         });
 
@@ -134,7 +153,7 @@ public class LoginActivity extends AppCompatActivity {
         animSet.play(headerMoveAnim).with(usernameFadeAnim).with(passwordFadeAnim).with(btnFadeAnim).with(noAccountFadeAnim);
 
         animSet.start();
-        findViewById(R.id.loginHeader).startAnimation(headerTextAnim);
+        loginHeader.startAnimation(headerTextAnim);
     }
 
 }
