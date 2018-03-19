0e99992f8eb21dca1d009d326d514b4832706e7d
Niels Heltner <niels.heltner@hotmail.com>
2018-03-19 20:01:16 +0100

    Added error handling and messages

--- app/src/main/java/io/beskedr/gui/RegisterActivity.java
+++ app/src/main/java/io/beskedr/gui/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -27,9 +28,13 @@ import io.beskedr.domain.User;
 public class RegisterActivity extends AppCompatActivity {
 
     @BindView(R.id.registerUsername) EditText usernameField;
+    @BindView(R.id.textInputLayoutRegisterUsername) TextInputLayout usernameLayout;
     @BindView(R.id.registerEmail) EditText emailField;
+    @BindView(R.id.textInputLayoutRegisterEmail) TextInputLayout emailLayout;
     @BindView(R.id.registerName) EditText nameField;
+    @BindView(R.id.textInputLayoutRegisterName) TextInputLayout nameLayout;
     @BindView(R.id.registerPassword) EditText passwordField;
+    @BindView(R.id.textInputLayoutRegisterPassword) TextInputLayout passwordLayout;
     private DatabaseReference database;
 
     @Override
@@ -79,44 +84,75 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void register(View view) {
-        Toast.makeText(getApplicationContext(), R.string.toast_register_success, Toast.LENGTH_SHORT).show();
+        clearErrorMessages();
 
-        usernameField.setError("fuck du er dum");
+        final Intent registeredIntent = new Intent(this, LoginActivity.class);
+        final String username = usernameField.getText().toString();
+        final String email = emailField.getText().toString();
+        final String name = nameField.getText().toString();
+        final String password = passwordField.getText().toString();
 
-        ((TextInputLayout) findViewById(R.id.textInputLayoutRegisterEmail)).setError("lmao wtf");
-
-        Intent registeredIntent = new Intent(this, LoginActivity.class);
-        String username = usernameField.getText().toString();
-        String email = emailField.getText().toString();
-        String name = nameField.getText().toString();
-        String password = passwordField.getText().toString();
-
-        User newUser = new User(username, email, name, password);
-
-        //database.push().setValue(newUser);
-        database.child(username).setValue(newUser);
-
-
-
-        /*database.addListenerForSingleValueEvent(new ValueEventListener() {
+        database.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                //for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                if(dataSnapshot.hasChild(username)) {
+                    showErrorMessage(usernameLayout, getString(R.string.error_registration_username_taken));
+                }
+                else if(username.length() < 4) {
+                    showErrorMessage(usernameLayout, getString(R.string.error_registration_username_too_short));
+                }
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    showErrorMessage(emailLayout, getString(R.string.error_registration_email_invalid));
+                }
+                if(name.length() < 2) {
+                    showErrorMessage(nameLayout, getString(R.string.error_registration_name_too_short));
+                }
+                if(password.length() < 3) {
+                    showErrorMessage(passwordLayout, getString(R.string.error_registration_password_too_short));
+                }
+                else {
+                    User newUser = new User(username, email, name, password);
+                    createNewUser(newUser);
+                    startLoginActivity(registeredIntent, newUser);
+                }
+                /*for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
                     User user = dataSnapshot.getValue(User.class);
                     //Log.d("Database", dataSnapshot.getChildrenCount() + "");
                     Log.d("Database", user.getUsername() + "");
-                //}
+                }*/
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
             }
-        });*/
+        });
+    }
 
-        registeredIntent.putExtra(getString(R.string.EXTRA_USERNAME), username);
-        registeredIntent.putExtra(getString(R.string.EXTRA_PASSWORD), password);
-        //startActivity(registeredIntent);
+    private void createNewUser(User newUser) {
+        database.child(newUser.getUsername()).setValue(newUser);
+        Toast.makeText(getApplicationContext(), R.string.toast_register_success, Toast.LENGTH_SHORT).show();
+    }
+
+    private void startLoginActivity(Intent intent, User newUser) {
+        intent.putExtra(getString(R.string.EXTRA_USERNAME), newUser.getUsername());
+        intent.putExtra(getString(R.string.EXTRA_PASSWORD), newUser.getPassword());
+        startActivity(intent);
+    }
+
+    private void showErrorMessage(TextInputLayout view, String errorMessage) {
+        view.setError(errorMessage);
+    }
+
+    private void clearErrorMessages() {
+        clearErrorMessage(usernameLayout);
+        clearErrorMessage(emailLayout);
+        clearErrorMessage(nameLayout);
+        clearErrorMessage(passwordLayout);
+    }
+
+    private void clearErrorMessage(TextInputLayout view) {
+        showErrorMessage(view, null);
     }
 
 }
