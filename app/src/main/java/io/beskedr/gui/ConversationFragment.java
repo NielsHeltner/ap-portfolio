f647b47d63718ff384dbd095af59e4ef27a3980e
Niels Heltner <niels.heltner@hotmail.com>
2018-04-04 16:36:37 +0200

    Restored ConversationFragment

--- app/src/main/java/io/beskedr/gui/ConversationFragment.java
+++ app/src/main/java/io/beskedr/gui/ConversationFragment.java
@@ -2,6 +2,7 @@ package io.beskedr.gui;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,12 +10,22 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AnimationUtils;
-import android.view.animation.LayoutAnimationController;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
 import java.util.List;
+import java.util.TimeZone;
 
 import io.beskedr.R;
 import io.beskedr.domain.ConversationMessage;
@@ -23,23 +34,89 @@ import io.beskedr.domain.UserManager;
 
 public class ConversationFragment extends Fragment {
 
-    private static int TEST_FOR_MESSAGES = 0;
-    private final User me = new User("Niels", "niels@heltner.net", "Niels Heltner", "123");
-    private final User you = new User("Lars", "lars@christensen.net", "Lars Christensen", "123");
-
     private RecyclerView conversationMessageView;
     private RecyclerView.Adapter conversationAdapter;
+    private DatabaseReference usersRef;
+    private DatabaseReference otherUserRef;
+    private DatabaseReference messagesRef;
     private List<ConversationMessage> conversationMessages;
     private User other;
+    private String convoId;
 
     public ConversationFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        conversationMessages = new ArrayList<>();
+        other = (User) getArguments().getSerializable(getString(R.string.EXTRA_OTHER_USER));
+
+        usersRef = FirebaseDatabase.getInstance().getReference().child("users");
+        otherUserRef = usersRef.child(UserManager.getInstance().getCurrentUser().getUsername()).child("contacts").child(other.getUsername());
+        messagesRef = FirebaseDatabase.getInstance().getReference().child("messages");
+
+        otherUserRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                convoId = dataSnapshot.getValue(String.class);
+
+                messagesRef.child(convoId).addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        final ConversationMessage conversationMessage = dataSnapshot.getValue(ConversationMessage.class);
+                        if(conversationMessage.getTime() == 0) {
+                            return;
+                        }
+
+                        usersRef.child(conversationMessage.getUser()).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                User sender = dataSnapshot.getValue(User.class);
+                                conversationMessage.setSender(sender);
+                                addMessage(conversationMessage);
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         AppCompatActivity activity = (AppCompatActivity) getActivity();
         activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        other = (User) getArguments().getSerializable(getString(R.string.EXTRA_OTHER_USER));
         activity.getSupportActionBar().setTitle(other.getName());
 
         View view = inflater.inflate(R.layout.fragment_conversation, container, false);
@@ -47,19 +124,12 @@ public class ConversationFragment extends Fragment {
         conversationMessageView = view.findViewById(R.id.conversationMessageView);
         conversationMessageView.setHasFixedSize(true);
         conversationMessageView.setLayoutManager(new LinearLayoutManager(getContext()));
-
-
-
-
-        UserManager.getInstance().setCurrentUser(me);
-        conversationMessages = new ArrayList<>();
-
-        conversationMessages.add(new ConversationMessage(me, "Hej!", "12:00"));
-        conversationMessages.add(new ConversationMessage(you, "hej", "12:01"));
-        conversationMessages.add(new ConversationMessage(me, "Hvordan går det?", "12:03"));
-        conversationMessages.add(new ConversationMessage(you, "jamen jeg har det egentligt rigtig godt i dag men så kom jeg til at møde anton og fandt ud hvor flot han egentlig var og så fik jeg bare sådan rigtig mange mangemangemange mange mindreværdskomplekser men så fandt jeg ud af han faktisk var grim og så fik jeg det godt igen", "12:28"));
-        conversationMessages.add(new ConversationMessage(me, "jamen jeg har det egentligt rigtig godt i dag men så kom jeg til at møde anton og fandt ud hvor flot han egentlig var og så fik jeg bare sådan rigtig mange mangemangemange mange mindreværdskomplekser men så fandt jeg ud af han faktisk var grim og så fik jeg det godt igen", "12:28"));
-        conversationMessages.add(new ConversationMessage(you, "jamen jeg har det egentligt rigtig godt i dag men så kom jeg til at møde anton og fandt ud hvor flot han egentlig var og så fik jeg bare sådan rigtig mange mangemangemange mange mindreværdskomplekser men så fandt jeg ud af han faktisk var grim og så fik jeg det godt igen", "12:28"));
+        conversationMessageView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                conversationMessageView.scrollToPosition(conversationMessages.size() - 1);
+            }
+        });
 
         conversationAdapter = new ConversationAdapter(getContext(), conversationMessages);
         conversationMessageView.setAdapter(conversationAdapter);
@@ -76,17 +146,29 @@ public class ConversationFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.app_name));
+    }
+
     public void sendMessage() {
-        Toast.makeText(getContext(), "send message", Toast.LENGTH_SHORT).show();
-        if(TEST_FOR_MESSAGES % 2 == 0) {
-            conversationMessages.add(new ConversationMessage(me, "test", "14:05"));
+        EditText messageBox = getView().findViewById(R.id.conversationEditText);
+        String message = messageBox.getText().toString().trim();
+        if(!message.isEmpty()) {
+            messagesRef.child(convoId).push().setValue(new ConversationMessage(UserManager.getInstance().getCurrentUser().getUsername(), message, Calendar.getInstance(TimeZone.getDefault()).getTimeInMillis())).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getContext(), R.string.error_convo_send_message, Toast.LENGTH_SHORT).show();
+                }
+            });
+            messageBox.setText("");
         }
-        else {
-            conversationMessages.add(new ConversationMessage(you, "test", "14:05"));
-        }
-        TEST_FOR_MESSAGES++;
+    }
 
-
+    private void addMessage(ConversationMessage message) {
+        conversationMessages.add(message);
+        Collections.sort(conversationMessages);
         updateRecyclerViewPan();
     }
 
